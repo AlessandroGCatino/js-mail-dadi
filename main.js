0d32c46b1fc586b3acd6a74cda5690db59a3cdec
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-01-10 16:57:25 +0100

    conclusione esercizio mail

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const playButton = document.getElementById("play")
 playButton.addEventListener("click", throwDice)
 
+const checkButton = document.getElementById("check")
+checkButton.addEventListener("click", controllaMail)
+
+const addButton = document.getElementById("add")
+addButton.addEventListener("click", aggiungiMail)
+
+const mailOut = document.getElementById("mailOutput")
+
+const listaMail = ["prova@gmail.com", "mail@gmail.com", "mailreale@gmail.com", "placeholder@mail.com", "test@mail.it"]
+
+
 function throwDice(){
     let player = randomNumber(1,6)
     let cpu = randomNumber(1,6)
@@ -13,21 +24,50 @@ function throwDice(){
     cpuRoll.innerHTML = cpu
     
     if(player>cpu){
-        console.log("Hai vinto!")
+        console.log("Win")
         results.innerHTML = "Hai vinto!"
         results.classList = "text-success"
 
     } else if (cpu>player){
-        console.log("Hai perso!")
+        console.log("Lose")
         results.innerHTML = "Hai perso!"
         results.classList = "text-danger"
 
     } else {
-        console.log("Pareggio!")
+        console.log("Draw")
         results.innerHTML = "Pareggio!"
         results.classList = "text-warning"
-
     }
+}
+
+function controllaMail(){
+    let mailUtente = document.getElementById("userMail").value
+    for (let i=0; i < listaMail.length; i++){
+        if(mailUtente == listaMail[i]){
+            mailOut.innerHTML = "Mail trovata"
+        } else {
+            mailOut.innerHTML = "Mail non presente"
+        }
+    }
+}
+
+function aggiungiMail(){
+    let mailUtente = document.getElementById("userMail").value
+    let flag = false
+
+    for (let i=0; i < listaMail.length; i++){
+
+        if(mailUtente == listaMail[i]){
+            flag=true
+        }
+    }
+    if(flag){
+        mailOut.innerHTML = "Mail già presente, inseriscine un'altra"
+    } else {
+        listaMail.push(mailUtente)
+        mailOut.innerHTML = "Nuova mail inserita!"
+    }
+    console.log(listaMail)
 }
 
 function randomNumber (min, max){
